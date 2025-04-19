7b0e44c2b0107a111bdf71dd5eab91c1a556f1c5
M_FOR_MAHIR <mahirsiam2004@gmail.com>
2025-04-19 19:02:42 +0600

    fixed insert any position

--- Singly Linked List/InsertAnyPosition.cpp
+++ Singly Linked List/InsertAnyPosition.cpp
@@ -1,7 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
-
-class Node // একটা node class create করবো।
+#define ll long long
+class Node
 {
 public:
     int val;
@@ -12,21 +12,42 @@ public:
         this->next = NULL;
     }
 };
-
-
-void insert_at_position(Node *&head, int index, int value)
-{
-
-    Node *newNode = new Node(value); // নতুন Node বানিয়ে নিলাম 
-
-    Node *temp = head; // temp পয়েন্টার এ head রেখে দিলাম 
-
-    for (int i = 1; i <= index - 1; i++)
-    {
-        temp = temp->next;
+void insertAtTail(Node *&head,int val){
+    Node *newNode=new Node(val);
+    Node *temp=head;
+    if(head==nullptr){
+        head=newNode;
+        return;
     }
-    // এখানে temp এ যে index এ Node insert করতে হবে তার previous Node আছে
-    // temp->next এ আছে তার পরের Node 
-    newNode->next = temp->next; // New_node এর next পয়েন্টার এ  next_node কে রেখে দিলাম
-    temp->next = newNode; //Previous_Node এর next পয়েন্টার এ  new_node এর এড্রেস রেখে দিলাম
+    while(temp->next!=nullptr){
+        temp=temp->next;
+    }
+    temp->next=newNode;
+
+}
+
+void insertAnyPosition(Node *head,int index,int val){
+    Node *newNode=new Node(val);
+    Node *temp=head;
+    for(int i=1;i<=index-1;i++){
+        temp=temp->next;
+    }
+    newNode->next=temp->next;
+    temp->next=newNode;
+}
+void print(Node *head){
+    Node *temp=head;
+    while(temp!=nullptr){
+        cout<<temp->val<<" ";
+        temp=temp->next;
+    }
+}
+int main()
+{
+    Node *head = nullptr;
+    insertAtTail(head, 10);
+    insertAtTail(head, 20);
+    insertAnyPosition(head,2,30);
+    print(head);
+    return 0;
 }
--- Singly Linked List/InsertAt_Tail.cpp
+++ Singly Linked List/InsertAt_Tail.cpp
@@ -1,42 +1,51 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 #define ll long long
 class Node
 {
-    public:
-        int val;
-        Node* next;
+public:
+    int val;
+    Node *next;
     Node(int val)
     {
-        this->val=val;
-        this->next=NULL;
+        this->val = val;
+        this->next = NULL;
     }
 };
-void insertAtTail(Node *&head,int val){
-    Node *newNode=new Node(val);
-    Node *temp =head;
-    if(head==nullptr){
-        head=newNode;
-        return ;
+void insertAtTail(Node *&head, int val)
+{
+    Node *newNode = new Node(val);
+    Node *temp = head;
+    if (head == nullptr)
+    {
+        head = newNode;
+        return;
     }
-    while(temp->next!=nullptr){
-        temp=temp->next;
+    while (temp->next != nullptr)
+    {
+        temp = temp->next;
     }
-    temp->next=newNode;
+    temp->next = newNode;
 }
 
-void print(Node *head){
-    Node *temp=head;
-    while(temp!=nullptr){
-        cout<<temp->val<<" ";
-        temp=temp->next;
+void print(Node *head)
+{
+    Node *temp = head;
+    while (temp != nullptr)
+    {
+        cout << temp->val << " ";
+        temp = temp->next;
     }
     cout << endl;
 }
-int main(){
-    Node *head=nullptr;
-    insertAtTail(head,10);
-    insertAtTail(head,20);
+int main()
+{
+    Node *head = nullptr;
+    insertAtTail(head, 10);
+    insertAtTail(head, 20);
+    insertAtTail(head, 30);
+    insertAtTail(head, 40);
+    insertAtTail(head, 50);
     print(head);
     return 0;
 }
