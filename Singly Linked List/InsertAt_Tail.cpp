6734146727cc6dc9af8d17c2933c3c10d2330859
M_FOR_MAHIR <mahirsiam2004@gmail.com>
2025-04-19 18:41:48 +0600

    re write and practise insertAtTail.cpp

--- Singly Linked List/InsertAt_Tail.cpp
+++ Singly Linked List/InsertAt_Tail.cpp
@@ -1,57 +1,42 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
-
-class Node // একটা node class create করবো।
+#define ll long long
+class Node
 {
-public:
-    int val;
-    Node *next;
+    public:
+        int val;
+        Node* next;
     Node(int val)
     {
-        this->val = val;
-        this->next = NULL;
+        this->val=val;
+        this->next=NULL;
     }
 };
-
-void insert_at_tail(Node *&head, int val) // reference সহ পয়েন্টার পাস করছি যাতে করে main এর head পয়েন্টার change হয়ে যায়
-{
-
-    Node *newNode = new Node(val); // নতুন একটি Node ক্রিয়েট করলাম
-    
-    // Important : Exception Handling 
-    if (head == NULL) // যদি লিঙ্কড লিস্ট খালি থাকে তবে এই নোড কে head বানিয়ে দিলাম
-    {
-        head = newNode;
-        return;
+void insertAtTail(Node *&head,int val){
+    Node *newNode=new Node(val);
+    Node *temp =head;
+    if(head==nullptr){
+        head=newNode;
+        return ;
     }
-
-    Node *temp = head; // লুপের জন্য temp পয়েন্টার নিলাম
-
-    while (temp->next != NULL) // যতক্ষন tail এ আসছি না , ততক্ষন লুপ চলবে
-    {
-        temp = temp->next; // প্রতি লুপে temp কে ডানে অর্থাৎ পরবর্তী Node এ shift করছি
+    while(temp->next!=nullptr){
+        temp=temp->next;
     }
-
-    temp->next = newNode; // tail এর next পয়েন্টার এ নতুন Node এর এড্রেস রেখে দিচ্ছি এবং এর মাধ্যমে Node টি লিঙ্কড লিস্টে এড হয়ে যাবে। 
+    temp->next=newNode;
 }
-void print(Node *head)
-{
-    Node *temp = head;
 
-    while (temp != NULL)
-    {
-        cout << temp->val << " ";
-        temp = temp->next;
+void print(Node *head){
+    Node *temp=head;
+    while(temp!=nullptr){
+        cout<<temp->val<<" ";
+        temp=temp->next;
     }
     cout << endl;
 }
-
-int main()
-{
-
-    Node *head = NULL;
-    insert_at_tail(head, 1);
-    insert_at_tail(head, 2);
-
+int main(){
+    Node *head=nullptr;
+    insertAtTail(head,10);
+    insertAtTail(head,20);
     print(head);
+    return 0;
 }
