9c33644604f4745f35f621f8a850e55ec1e5f53f
M_FOR_MAHIR <mahirsiam2004@gmail.com>
2025-04-19 15:06:59 +0600

    re written contructor

--- Singly Linked List/constructor.cpp
+++ Singly Linked List/constructor.cpp
@@ -1,33 +1,27 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
+#define ll long long
 class Node
 {
-    public:
-        int val;
-        Node* next;
-    Node(int val) // constructor এ  value টা নিচ্ছি।
+public:
+    int val;
+    Node *next;
+    Node(int val)
     {
-        this->val=val; // value টা class variable এ assign করছি।
-        this->next=NULL; // যেহেতু প্রতিটি nodeই আলাদা তাই by default zero/NULL set করে দিচ্ছি।
+        this->val = val;
+        this->next = nullptr;
     }
 };
-
-
 int main()
 {
-    Node a(10); // এখানে a object এ 10 value এর একটা node create করছি।
-    Node b(20); // এখানে b object এ 20 value এর একটা node create করছি।
-
-
-    a.next=&b;  //এখানে b node এর address a এর next/pointer এ রাখা হচ্ছে। যাতে a দিয়ে b কে access করা যায়।
-
-
-    cout<<a.val<<endl;  // এখানে a এর value print করে দেখলাম।
-    cout<<b.val<<endl; // এখানে b এর value print করে দেখলাম।
-    cout<<(*a.next).val<<endl; // এখানে b এর value a কে dereference করে print করে দেখলাম।
-    cout<<a.next->val<<endl; // এখানে b এর value a কে অন্যভাবে dereference করে print করে দেখলাম।
-
-
+    Node a(10);
+    Node b(20);
+    a.next = &b;
+    cout << "value of a is " << a.val << endl;
+    cout << "address of b is stored in a.next " << a.next << endl;
+    cout << "value of b useing (*a.next).val " << (*a.next).val << endl;
+    cout << "value of b useing a.next->val " << a.next->val << endl;
+    cout << "value of b is " << b.val << endl;
+    cout << b.next << endl;
     return 0;
 }
-
