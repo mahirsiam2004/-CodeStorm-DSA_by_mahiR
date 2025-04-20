ea66c0efa3f7e25f558f0652eed9133d28ccf239
M_FOR_MAHIR <mahirsiam2004@gmail.com>
2025-04-20 19:38:59 +0600

    added extra stuffs in test.cxx

--- Double Linked List/Test.cxx
+++ Double Linked List/Test.cxx
@@ -1,63 +1,87 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 #define ll long long
 class Node
 {
-    public:
-        int val;
-        Node* next;
-        Node* prev;
+public:
+    int val;
+    Node *next;
+    Node *prev;
     Node(int val)
     {
-        this->val=val;
-        this->next=nullptr;
-        this->prev=nullptr;
+        this->val = val;
+        this->next = nullptr;
+        this->prev = nullptr;
     }
 };
-void print(Node *head){
-    Node *temp=head;
-    while(temp!=nullptr){
-        cout<<temp->val<<" ";
-        temp=temp->next;
+void print(Node *head)
+{
+    Node *temp = head;
+    while (temp != nullptr)
+    {
+        cout << temp->val << " ";
+        temp = temp->next;
     }
-    cout<<endl;
+    cout << endl;
 }
 
-void printReverse(Node *tail){
-    Node *temp=tail;
-    while(temp!=nullptr){
-        cout<<temp->val<<" ";
-        temp=temp->prev;
+void printReverse(Node *tail)
+{
+    cout << "Print Reserse -> ";
+    Node *temp = tail;
+    while (temp != nullptr)
+    {
+        cout << temp->val << " ";
+        temp = temp->prev;
     }
-    cout<<endl;
+    cout << endl;
 }
 
-void insertAnyPosition(Node *head,int index,int val){
-    Node *newNode=new Node(val);
-    Node *temp=head;
-    for(int i=1;i<=index;i++){
-        temp=temp->next;
+void insertAtHead(Node *&head, Node *&tail, int val)
+{
+    Node *newNode = new Node(val);
+    if (head == nullptr)
+    {
+        head = newNode;
+        tail = newNode;
     }
-    newNode->next=temp->next;
-    temp->next=newNode;
-    newNode->next->prev=newNode;
-    newNode->prev=temp;
+    newNode->next = head;
+    head->prev = newNode;
+    head = newNode;
 }
 
-int main(){
-    Node *head=new Node(10);
-    Node *a=new Node(20);
-    Node *b=new Node(30);
-    Node *c=new Node(40);
-    Node *tail=c;
-head->next=a;
-a->prev=head;
-a->next=b;
-b->prev=a;
-b->next=c;
-c->prev=b;
+void insertAnyPosition(Node *head, int index, int val)
+{
+    Node *newNode = new Node(val);
+    Node *temp = head;
+    for (int i = 1; i <= index; i++)
+    {
+        temp = temp->next;
+    }
+    newNode->next = temp->next;
+    temp->next = newNode;
+    newNode->next->prev = newNode;
+    newNode->prev = temp;
+}
+
+int main()
+{
+    Node *head = new Node(10);
+    Node *a = new Node(20);
+    Node *b = new Node(30);
+    Node *c = new Node(40);
+    Node *tail = c;
+    head->next = a;
+    a->prev = head;
+    a->next = b;
+    b->prev = a;
+    b->next = c;
+    c->prev = b;
 
     print(head);
     printReverse(tail);
+    insertAnyPosition(head, 2, 400); // index based
+    insertAtHead(head, tail, 100);
+    print(head);
     return 0;
 }
