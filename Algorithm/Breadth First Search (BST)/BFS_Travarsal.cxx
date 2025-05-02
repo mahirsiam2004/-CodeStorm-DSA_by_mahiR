e6e5da88d5b5bcb5c3d67d385e372f3f940aa714
M_FOR_MAHIR <mahirsiam2004@gmail.com>
2025-05-02 16:12:58 +0600

    added bfs

---
+++ Algorithm/Breadth First Search (BST)/BFS_Travarsal.cxx
@@ -0,0 +1,8 @@
+//  Breadth First Search (BFS) is a graph traversal algorithm that explores the neighbor nodes at the present depth prior to moving on to nodes at the next depth level. It is commonly used for finding the shortest path in unweighted graphs.It also called Single Source Shortest Path Algorithm.
+#include<bits/stdc++.h>
+using namespace std;
+vector<int>v[1005];
+int main(){
+    
+    return 0;
+}
